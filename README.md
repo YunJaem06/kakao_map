7c122fc04fd59f4c5a225b34e76d4805af35640f
YunJaem06 <yunjae0616@gmail.com>
2022-10-27 19:51:12 +0900

    kakao map 오류 해결

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # kakao_map
+---
+## kakao map api 적용하면서 생긴 문제 해결
+* UnsatisfiedLinkError 에러가 발생
+* kakao map 은 에뮬레이터에서 동작하지 않는다.
+* 안드로이드 기기를 이용하는것이 좋다.
--- app/src/main/java/com/example/kakao_map/MainActivity.kt
+++ app/src/main/java/com/example/kakao_map/MainActivity.kt
@@ -1,26 +1,26 @@
 package com.example.kakao_map
 
-import android.content.pm.PackageManager
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Base64
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import com.example.kakao_map.databinding.ActivityMainBinding
 import net.daum.mf.map.api.MapView
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         val mapView = MapView(this)
 
-        val mapViewContainer = findViewById<View>(R.id.map_view) as ViewGroup
-        mapViewContainer.addView(mapView)
+        binding.mapView.addView(mapView)
 
     }
 }
